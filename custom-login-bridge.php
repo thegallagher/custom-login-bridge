97c24bfa0c3a674a390b8ddb5101b537256eff3d
David Gallagher <david@thegallagher.net>
2014-03-12 15:03:08 +1100

    Add PhpDoc comments and use Wordpress conventions

--- custom-login-bridge.php
+++ custom-login-bridge.php
@@ -9,41 +9,54 @@
  * License: MIT
  */
 
-// Get the bridge user.
+/**
+ * Get the bridge user
+ *
+ * @return bool|WP_User
+ */
 function custom_login_bridge_get_bridge_user() {
 	$login = 'client';
-	if (defined('CUSTOM_LOGIN_BRIDGE_USER')) {
+	if ( defined( 'CUSTOM_LOGIN_BRIDGE_USER' ) ) {
 		$login = CUSTOM_LOGIN_BRIDGE_USER;
 	}
 
-	return get_user_by('login', $login);
+	return get_user_by( 'login', $login );
 }
 
-// Adds authentication for bridged users.
-add_filter('authenticate', 'custom_login_bridge_authenticate', 50, 3);
-function custom_login_bridge_authenticate($user, $username, $password) {
-	if (is_a($user, 'WP_User')) {
+/**
+ * Adds authentication for bridged users
+ *
+ * @param WP_User $user
+ * @param string $username
+ * @param string $password
+ * @return bool|null|WP_User
+ */
+function custom_login_bridge_authenticate( $user, $username, $password ) {
+	if ( is_a( $user, 'WP_User' ) ) {
 		return $user;
 	}
 
-	if (!function_exists('custom_login_bridge_validate')) {
+	if ( ! function_exists( 'custom_login_bridge_validate' ) ) {
 		return null;
 	}
 
-	if (custom_login_bridge_validate($username, $password)) {
+	if ( custom_login_bridge_validate( $username, $password ) ) {
 		return custom_login_bridge_get_bridge_user();
 	}
 
 	return null;
 }
+add_filter( 'authenticate', 'custom_login_bridge_authenticate', 50, 3 );
 
-// Prevents bridge the user from accessing the admin section.
-add_action('admin_init', 'custom_login_bridge_block_dashboard');
+/**
+ * Prevents bridge user from accessing the admin section.
+ */
 function custom_login_bridge_block_dashboard() {
 	$user = wp_get_current_user();
 	$bridge_user = custom_login_bridge_get_bridge_user();
-	if ($user->user_login === $bridge_user->user_login && (!defined('DOING_AJAX') || DOING_AJAX)) {
-		wp_redirect(home_url());
+	if ( $user->user_login === $bridge_user->user_login && ( ! defined( 'DOING_AJAX' ) || DOING_AJAX ) ) {
+		wp_redirect( home_url() );
 		exit;
 	}
 }
+add_action( 'admin_init', 'custom_login_bridge_block_dashboard' );
