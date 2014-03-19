950f9cf03064697b34969b7e3db2aa8d7b61eff4
David Gallagher <david@thegallagher.net>
2014-03-19 13:52:32 +1100

    Add useful shortcodes

--- custom-login-bridge.php
+++ custom-login-bridge.php
@@ -60,3 +60,109 @@ function custom_login_bridge_block_dashboard() {
 	}
 }
 add_action( 'admin_init', 'custom_login_bridge_block_dashboard' );
+
+/**
+ * Shortcode to display content only to specified users
+ *
+ * @param $atts
+ * @param null $content
+ * @return null|string
+ */
+function custom_login_bridge_if_user_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'id'    => null,
+		'login' => null,
+		'roles' => null,
+		'none'  => null,
+	), $atts, 'clb-if-user' ) );
+
+	$display = true;
+	$current_user = wp_get_current_user();
+	if ( !empty( $id ) ) {
+		$ids = explode( ',', $id );
+		$display = in_array( $current_user->ID, $ids );
+	} elseif ( ! empty( $login ) ) {
+		$logins = explode( ',', $login );
+		$display = in_array( $current_user->user_login, $logins );
+	} elseif ( ! empty( $roles ) ) {
+		$roles = explode( ',', $roles );
+		$user_role = array_shift( $current_user->roles );
+		$display = in_array ( $user_role, $roles );
+	} elseif ( ! empty( $none ) ) {
+		$display = 0 == $current_user->ID;
+	}
+
+	if ( $display ) {
+		return do_shortcode($content);
+	}
+	return null;
+}
+add_shortcode( 'clb-if-user', 'custom_login_bridge_if_user_shortcode' );
+
+/**
+ * Shortcode to display user information
+ *
+ * @param $atts
+ * @return string|null
+ */
+function custom_login_bridge_user_info_shortcode( $atts ) {
+	extract( shortcode_atts( array(
+		'field' => 'login',
+	), $atts, 'clb-user-info' ) );
+
+	$current_user = wp_get_current_user();
+	$field = array_shift( $atts );
+	switch ( $field ) {
+		case 'roles':
+		case 'caps':
+		case 'allcaps':
+			return implode( ',', $current_user->{$field} );
+	}
+
+	if ( isset( $current_user->{$field} ) ) {
+		return $current_user->{$field};
+	}
+	return null;
+}
+add_shortcode( 'clb-user-info', 'custom_login_bridge_user_info_shortcode' );
+
+/**
+ * Shortcode to display a login form
+ *
+ * A filter can be applied to the form after it has been generated.
+ *
+ * @see wp_login_form() used to generate the form. Attributes are passed to the function.
+ *
+ * @param $atts
+ * @return string
+ */
+function custom_login_bridge_login_form_shortcode( $atts ) {
+	$atts = shortcode_atts( array(
+		'redirect'       => $_SERVER['REQUEST_URI'],
+		'form_id'        => 'loginform',
+		'label_username' => 'Username',
+		'label_password' => 'Password',
+		'label_remember' => 'Remember Me',
+		'label_log_in'   => 'Log In',
+		'id_username'    => 'user_login',
+		'id_password'    => 'user_pass',
+		'id_remember'    => 'rememberme',
+		'id_submit'      => 'wp-submit',
+		'remember'       => 'true',
+		'value_username' => null,
+		'value_remember' => 'false',
+	), $atts, 'clb-login-form' );
+
+	$atts['echo']           = false;
+	$atts['redirect']       = site_url( $atts['redirect'] );
+	$atts['label_username'] = __( $atts['label_username'] );
+	$atts['label_password'] = __( $atts['label_password'] );
+	$atts['label_remember'] = __( $atts['label_remember'] );
+	$atts['label_log_in']   = __( $atts['label_log_in'] );
+	$atts['remember']       = 'true' ==  $atts['remember'];
+	$atts['value_remember'] = 'true' ==  $atts['value_remember'];
+
+	$form = wp_login_form( $atts );
+	return apply_filters( 'custom_login_bridge_login_form', $form );
+}
+add_shortcode( 'clb-login-form', 'custom_login_bridge_login_form_shortcode' );
